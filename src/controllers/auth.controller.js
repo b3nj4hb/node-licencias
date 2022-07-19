64e16a7b0197f166f921438900d69201ac58cc71
b3nj4hb <benjalive12@gmail.com>
2022-07-19 02:58:57 -0500

    jwt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,46 +1,47 @@
+import { response } from "express";
 import { conn } from "../database";
-const controller = {}
 
+const controller = {}
 const jwt = require('jsonwebtoken');
-const secret = "leidy-decret-access-token";
-const refreshTokenSecret = "leidy-decret-refresh-access-token";
-const port = 2000;
-const tokenLife = 900;
-const refreshTokenLife = 86400;
-const helpers = require('../libs/helpers');
+const config = require('../libs/config.json');
 const bcrypt = require('bcryptjs');
 const refreshTokens = [];
 
+// var salt = bcrypt.genSaltSync(10);
+
+/** Encrypt password */
+/*bcrypt.hash('anypassword', salt, (err, res) => {
+    console.log('hash', res)
+    hash = res
+    compare(hash)
+});*/
+
 controller.login = async (req, res) => {
     const { username, password } = req.body;
-    console.log(username + password)
+    console.log('~~ post ~~\n' + 'username: ' + username + '\npassword: ' + password + '\n~~~~~~~~~~~~')
     //console.log(pass);
-    conn.query("select * from usuario where user = ?;", [username], function (err, result) {
+    conn.query("call sp_validar_usuario(?);", [username], function (err, result) {
         try {
+            // return res.status(200).json(result);
             if (result[0].length != 0) {
-                const passold = result[0].PASSWORD;
-                console.log(result[0].PASSWORD)
+                const passold = result[0][0].password;
                 if (bcrypt.compare(password, passold)) {
                     const user = {
-
-                        username: result[0].USER
+                        idper: result[0][0]["idpersona"],
+                        iduser: result[0][0]["idusuario"],
+                        user: result[0][0]["user"],
+                        rol: result[0][0]["rol"],
+                        nombres: result[0][0]["nombres"]
                     }
-                    const accessToken = jwt.sign({ user }, secret, { expiresIn: '32s' });
-                    const refreshToken = jwt.sign({ user }, refreshTokenSecret);
+                    const accessToken = jwt.sign({ user }, config.secret, { expiresIn: '32s' });
+                    const refreshToken = jwt.sign({ user }, config.refreshTokenSecret);
                     refreshTokens.push(refreshToken);
-                    return res.status(200).json({
-                        accessToken,
-                        refreshToken
-                    });
+                    return res.status(200).json({ accessToken, refreshToken });
                 } else {
-                    return res.status(200).json(
-                        'Username o Password incorrectos...!'
-                    );
+                    return res.status(200).json('Username o Password incorrectos...!');
                 }
             }
-            return res.status(200).json(
-                'Username o Password incorrectos...!'
-            );
+            return res.status(200).json('Username o Password incorrectos...!');
         } catch (error) {
             return res.status(500).json('Error al VALIDAR USUARIO ');
         }
