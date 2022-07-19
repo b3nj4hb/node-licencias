04aa7b4df84b9efa67a212fc05ad130a3971ee5c
b3nj4hb <benjalive12@gmail.com>
2022-07-19 12:01:17 -0500

    jwt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,25 +7,19 @@ const config = require('../libs/config.json');
 const bcrypt = require('bcryptjs');
 const refreshTokens = [];
 
-// var salt = bcrypt.genSaltSync(10);
-
-/** Encrypt password */
-/*bcrypt.hash('anypassword', salt, (err, res) => {
-    console.log('hash', res)
-    hash = res
-    compare(hash)
-});*/
-
 controller.login = async (req, res) => {
     const { username, password } = req.body;
     console.log('~~ post ~~\n' + 'username: ' + username + '\npassword: ' + password + '\n~~~~~~~~~~~~')
-    //console.log(pass);
     conn.query("call sp_validar_usuario(?);", [username], function (err, result) {
         try {
-            // return res.status(200).json(result);
-            if (result[0].length != 0) {
-                const passold = result[0][0].password;
-                if (bcrypt.compare(password, passold)) {
+            // Hashear password
+            // let passwordhashed = bcrypt.hashSync(password, 8);
+            // console.log({ message: passwordhashed })
+            const rUser = result[0][0].user;
+            if (rUser == username) {
+                const hashSaved = result[0][0].password;
+                let compare = bcrypt.compareSync(password, hashSaved);
+                if (compare) {
                     const user = {
                         idper: result[0][0]["idpersona"],
                         iduser: result[0][0]["idusuario"],
@@ -36,14 +30,30 @@ controller.login = async (req, res) => {
                     const accessToken = jwt.sign({ user }, config.secret, { expiresIn: '32s' });
                     const refreshToken = jwt.sign({ user }, config.refreshTokenSecret);
                     refreshTokens.push(refreshToken);
-                    return res.status(200).json({ accessToken, refreshToken });
+                    return res.json({ message: 'validacion exitosa', accessToken, refreshToken });
                 } else {
-                    return res.status(200).json('Username o Password incorrectos...!');
+                    return res.json({ message: 'no son iguales' });
                 }
+            } else {
+                return res.json({ message: 'Ingrese corretamente sus credenciales!' });
             }
-            return res.status(200).json('Username o Password incorrectos...!');
         } catch (error) {
-            return res.status(500).json('Error al VALIDAR USUARIO ');
+            return res.json({ message: '' + error });
+        }
+    });
+};
+
+controller.registrar = async (req, res) => {
+    let sql = "call sp_ins_usuario (?,?,?);"
+    const { usuario, password, idpersona } = req.body;
+    // Hashear password
+    let passwordhashed = bcrypt.hashSync(password, 8);
+    // console.log({ message: passwordhashed })
+    conn.query(sql, [usuario, passwordhashed, idpersona], function (err, result) {
+        try {
+            return res.json({ result });
+        } catch (error) {
+            return res.json({ e });
         }
     });
 };
@@ -51,21 +61,12 @@ controller.login = async (req, res) => {
 controller.token = async (req, res) => {
     try {
         const { token } = req.body;
-        if (!token) {
-            return res.sendStatus(401);
-        }
-        if (!refreshTokens.includes(token)) {
-            return res.sendStatus(403);
-        }
+        if (!token) { return res.sendStatus(401); }
+        if (!refreshTokens.includes(token)) { return res.sendStatus(403); }
         jwt.verify(token, config.refreshTokenSecret, (err, user) => {
-            if (err) {
-                return res.sendStatus(403);
-            }
+            if (err) { return res.sendStatus(403); }
         });
-    } catch (e) {
-        console.log(e);
-
-    }
+    } catch (e) { console.log(e); }
 };
 
 module.exports = controller;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const auth = require('../controllers/auth.controller');
 
 router.post('/', auth.login);
+router.post('/registrar', auth.registrar);
 
 export default router;
