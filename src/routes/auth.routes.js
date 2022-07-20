006cfe13a86f37a246a59b68c17d053a527f87a5
b3nj4hb <benjalive12@gmail.com>
2022-07-20 13:20:39 -0500

    sin jwt

--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-const auth = require('../controllers/auth.controller');
-
-router.post('/', auth.login);
-router.post('/registrar', auth.registrar);
-
-export default router;
